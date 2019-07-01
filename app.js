b16d7f3630227a4db5ed7648d571a81e3df83616
Dario Pirola <d.pirola@tiscali.it>
2019-07-01 21:35:12 +0200

    added morgan

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const debug = require('debug')('app');
 // set DEBUG=* & node app.js
 const chalk = require('chalk');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('combined'));
 
 app.get('/', function(req, res){
     res.send("Answering to the call!!");
